ef2e95423101a0f6bbc39486204b4b3a3b6963fa
Liberbell <liberbell@gmail.com>
2021-02-22 09:17:30 +0900

    views4

--- first/myapp/views.py
+++ first/myapp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello World', charset='CP932')
+    return HttpResponse('Hello World', charset='cp932')
