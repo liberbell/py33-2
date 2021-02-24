3887620a8879777d71c0331c916e76a6f4ea0345
Liberbell <liberbell@gmail.com>
2021-02-25 08:52:37 +0900

    views3

--- first/project01/diary/views.py
+++ first/project01/diary/views.py
@@ -4,3 +4,9 @@ from .forms import DayCreateForm
 # Create your views here.
 def index(request):
     return render(request, 'diary/day_list.html')
+
+def add(request):
+    context = {
+        'form': DayCreateForm()
+    }
+    return render(request, 'diary/day_form.html', context)
