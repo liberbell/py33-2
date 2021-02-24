8964c281f382c16af475059e13244be524d4b5ca
Liberbell <liberbell@gmail.com>
2021-02-24 09:09:18 +0900

    models2

--- first/project01/diary/models.py
+++ first/project01/diary/models.py
@@ -2,3 +2,6 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+class Day(models.Model):
+    title = models.CharField('Title', max_length=200)
+    text = models.TextField('Text')
