8d336f8c10c24ddfecfe9462d65646e37f7983c1
Liberbell <liberbell@gmail.com>
2021-02-25 09:18:58 +0900

    views11

--- first/project01/diary/views.py
+++ first/project01/diary/views.py
@@ -7,7 +7,7 @@ def index(request):
     context = {
         'day_list': Day.objects.all()
     }
-    return render(request)
+    return render(request, 'diary/day_list.html', context)
 
 def add(request):
     form = DayCreateForm(request.POST or None)
