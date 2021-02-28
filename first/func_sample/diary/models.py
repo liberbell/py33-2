618cdc392fdf3fa4e05b63e444706926254dd0b0
Liberbell <liberbell@gmail.com>
2021-02-28 16:25:44 +0900

    funcsample1

---
+++ first/func_sample/diary/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Day(models.Model):
+    title = models.CharField('Title', max_length=200)
+    text = models.TextField('Text')
+    date = models.DateTimeField('DATE', default=timezone.now)
