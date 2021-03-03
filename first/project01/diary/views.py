642baf497c0bb8c7d8d43890b83165cd0533e18f
Liberbell <liberbell@gmail.com>
2021-03-04 08:48:47 +0900

    views1

--- first/project01/diary/views.py
+++ first/project01/diary/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from django.views import generic
 from .forms import DayCreateForm
 from .models import Day
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 # def index(request):
@@ -61,17 +62,17 @@ class IndexView(generic.ListView):
     model = Day
     paginate_by = 3
 
-class AddView(generic.CreateView):
+class AddView(LoginRequiredMixin, generic.CreateView):
     model = Day
     form_class = DayCreateForm
     success_url = reverse_lazy('diary:index')
 
-class UpdateView(generic.UpdateView):
+class UpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Day
     form_class = DayCreateForm
     success_url = reverse_lazy('diary:index')
 
-class DeleteView(generic.DeleteView):
+class DeleteView(LoginRequiredMixin, eneric.DeleteView):
     model= Day
     success_url = reverse_lazy('diary:index')
 
