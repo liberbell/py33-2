a184809ce547ede721d0749df6922b22dea2e154
Liberbell <liberbell@gmail.com>
2021-03-06 15:11:09 +0900

    models2

--- first/project02/employee/models.py
+++ first/project02/employee/models.py
@@ -13,3 +13,4 @@ class Employee(models.Model):
     first_name = models.CharField('first_name', max_length=20)
     last_name = models.CharField('last_name', max_length=20)
     email = models.EmailField('email', blank=True)
+    department = models.ForeignKey(Department, verbose_name='department', on_delete=models.PROTECT,)
