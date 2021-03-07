9027fb80832675a2663edf53a49e33cf9273c97e
Liberbell <liberbell@gmail.com>
2021-03-07 14:11:44 +0900

    views2

--- first/project02/employee/views.py
+++ first/project02/employee/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Employee
+from .forms import SearchForm
 
 # Create your views here.
 class IndexView(generic.ListView):
     # template_name = 'employee/employee_list.html'
     model = Employee
+
+    def get_context_data(self):
+        context = super().get_context_data()
