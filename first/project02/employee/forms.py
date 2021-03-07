7f49f3c4168068749929b12990428541894ac694
Liberbell <liberbell@gmail.com>
2021-03-07 14:07:13 +0900

    forms2

--- first/project02/employee/forms.py
+++ first/project02/employee/forms.py
@@ -2,4 +2,4 @@ from django import forms
 from .models import Club, Department
 
 class SearchForm(forms.Form):
-    club = 
+    club = forms.ModelChoiceField(queryset=Club.objects, label='club_name', required=False)
