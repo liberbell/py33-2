bcf756b0d98752b81c0008d754b0010d686e06ce
Liberbell <liberbell@gmail.com>
2021-03-07 14:08:12 +0900

    forms3

--- first/project02/employee/forms.py
+++ first/project02/employee/forms.py
@@ -3,3 +3,5 @@ from .models import Club, Department
 
 class SearchForm(forms.Form):
     club = forms.ModelChoiceField(queryset=Club.objects, label='club_name', required=False)
+
+    department = forms.ModelChoiceField(queryset=Department.objects, label='department_name', required=False)
