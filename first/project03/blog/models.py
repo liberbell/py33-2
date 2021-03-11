a0b274c4483957d96fc48c82ea2d4def12e4fec5
Liberbell <liberbell@gmail.com>
2021-03-11 09:07:29 +0900

    models4

--- first/project03/blog/models.py
+++ first/project03/blog/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+
+class Category(models.Model):
+    # Blog category
+    name = models.CharField('category_name', max_length=255)
+
+    def __str__(self):
+        return self.name
+    
 class Post(models.Model):
     # Blog content
     title = models.CharField('title', max_length=255)
