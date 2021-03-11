a8b85b4751289e33eb3e3d445c6e46f31770d14a
Liberbell <liberbell@gmail.com>
2021-03-11 09:09:24 +0900

    models6

--- first/project03/blog/models.py
+++ first/project03/blog/models.py
@@ -16,3 +16,7 @@ class Post(models.Model):
     title = models.CharField('title', max_length=255)
     text = models.TextField('content')
     created_at = models.DateTimeField('created_date', default=timezone.now)
+
+    def __str__(self):
+        return self.name
+    
