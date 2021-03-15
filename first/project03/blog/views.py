66797b3ac73d8f497da9cd4b313d72ba8c4e6470
Liberbell <liberbell@gmail.com>
2021-03-15 09:08:11 +0900

    views4

--- first/project03/blog/views.py
+++ first/project03/blog/views.py
@@ -11,5 +11,5 @@ class IndexView(generic.ListView):
         queryset = Post.objects.order_by('-created_at')
         keyword = self.request.GET.get('keyword')
         if keyword:
-            queryset = queryset.filter(title='keyword')
+            queryset = queryset.filter(title=keyword)
         return queryset
