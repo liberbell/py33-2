fc024060f177e960ab2119a8bdb204fce2cc46c4
Liberbell <liberbell@gmail.com>
2021-03-18 08:47:46 +0900

    forms1

--- first/project03/blog/forms.py
+++ first/project03/blog/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from .models import Comment
 
 class CommentCreateForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
     class Meta:
         model = Comment
         fields = ('name', 'text')
