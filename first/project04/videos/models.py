aa7631a8bc7419142a1800bee87802f2657789cb
Liberbell <liberbell@gmail.com>
2021-03-19 09:00:55 +0900

    models2

--- first/project04/videos/models.py
+++ first/project04/videos/models.py
@@ -5,11 +5,11 @@ class Video(models.Model):
     """動画"""
 
     title = models.CharField('Video Title', max_length=255)
-    description = models.TextField('説明(空欄可)', blank=True)
-    thumbnail = models.ImageField('サムネイル(空欄可)', upload_to='thumbnails/', null=True, blank=True)  # /media/thumbnails/ファイル名
-    upload = models.FileField('ファイル', upload_to='uploads/%Y/%m/%d/')  # /media/uploads/2018/3/20/ファイル名
-    created_at = models.DateTimeField('作成日', auto_now_add=True)  # default=timezone.nowと違い、入力欄は表示されない
-    updated_at = models.DateTimeField('更新日', auto_now=True)  # 更新するたびにその日時が格納される
+    description = models.TextField('Descriptions(optional)', blank=True)
+    thumbnail = models.ImageField('Thumbnail(optional)', upload_to='thumbnails/', null=True, blank=True)  # /media/thumbnails/ファイル名
+    upload = models.FileField('File', upload_to='uploads/%Y/%m/%d/')  # /media/uploads/2018/3/20/ファイル名
+    created_at = models.DateTimeField('Created date', auto_now_add=True)  # default=timezone.nowと違い、入力欄は表示されない
+    updated_at = models.DateTimeField('Update date', auto_now=True)  # 更新するたびにその日時が格納される
 
     def __str__(self):
         return self.title
