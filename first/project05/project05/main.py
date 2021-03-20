841569e7a406f280557f34336e95ea9eb6577c60
Liberbell <liberbell@gmail.com>
2021-03-20 23:21:41 +0900

    main5

--- first/project05/project05/main.py
+++ first/project05/project05/main.py
@@ -17,3 +17,10 @@ def create_and_save():
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, train_size = 50000, test_size = 0
     )
+
+    clf = svm.SVC()
+    clf.fit(X_train, y_train)
+
+    with open('mnist', 'wb') as file:
+        pickle.dump(clf, file)
+    return clf
