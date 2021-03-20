3e5fa7b842f820a477b3baa5341feca40ddde272
Liberbell <liberbell@gmail.com>
2021-03-20 23:18:58 +0900

    main2

--- first/project05/project05/main.py
+++ first/project05/project05/main.py
@@ -8,3 +8,8 @@ def read():
     with open('mnist.pickle', 'rb') as file:
         clf = pickle.load(file)
     return clf
+
+def create_and_save():
+    mnist = datasets.fetch_mldata('MNIST original', data_home='image/')
+    X = mnist.data / 255
+    Y = mnist.target
