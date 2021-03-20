df47fa8a3963a570e81708af4ba09f7ee18d7162
Liberbell <liberbell@gmail.com>
2021-03-20 23:17:24 +0900

    main3

--- first/project05/project05/main.py
+++ first/project05/project05/main.py
@@ -4,3 +4,7 @@ from PIL import Image
 from sklearn import datasets, svm
 from sklearn.model_selection import train_test_split
 
+def read():
+    with open('mnist.pickle', 'rb') as file:
+        clf = pickle.load(file)
+    return clf
